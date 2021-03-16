867dbcec447a66c085e465ef5d4f0b3020b9619f
Numericlock <49630768+Numericlock@users.noreply.github.com>
2021-03-16 17:41:51 +0900

    見た目調整

--- src/components/Metronome.vue
+++ src/components/Metronome.vue
@@ -7,114 +7,113 @@
             <div v-for="n in oneMusicalBar" :key="n" :class="[{ 'beat-now': (n==beatNow+1) }, 'one-beat']"></div>
         </div>
 -->
-        <polygonMetronome :oneMusicalBar="oneMusicalBar" :beatAngle="beatAngle2" :beatNow="beatNow" :soundMusicalBar="soundMusicalBar"/>
+        <polygonMetronome :oneMusicalBar="oneMusicalBar" :beatAngle="beatAngle2" :beatNow="beatNow" :soundMusicalBar="soundMusicalBar" />
     </div>
 </template>
 
 <script>
-//import Hand from './Hand.vue'
-import polygonMetronome from './polygonMetronome.vue'
-const fs = window.fs;
-let source;
-let audioContext;
-let toArrayBuffer;
-let textfile;
-let gainNode;
-    export default{
+    //import Hand from './Hand.vue'
+    import polygonMetronome from './polygonMetronome.vue'
+    const fs = window.fs;
+    let source;
+    let audioContext;
+    let toArrayBuffer;
+    let textfile;
+    let gainNode;
+    export default {
         data() {
             return {
-                elapsedTime:0,
-                beat:"",
+                elapsedTime: 0,
+                beat: "",
                 isMetronomeRunning: false,
-                beatTime:0,
-                swingAngle:90,
-                beatAngle:0,
-                edgeLength:100,
-                vertex:5
+                beatTime: 0,
+                swingAngle: 90,
+                beatAngle: 0,
+                edgeLength: 100,
+                vertex: 5
             }
         },
         components: {
             polygonMetronome
         },
-        props:{
-            bpm:{
+        props: {
+            bpm: {
                 type: Number,
-                default:60
+                default: 60
             },
-            fps:{
+            fps: {
                 type: Number,
-                default:30
+                default: 30
             },
-            oneMusicalBar:{
+            oneMusicalBar: {
                 type: Number,
             },
-            soundMusicalBar:{
+            soundMusicalBar: {
                 type: Boolean,
-                default:true
+                default: true
             }
         },
         computed: {
             pendulumAngle() {
                 const swingAngle = 90;
-                const swingProgress = this.elapsedTime%this.oneBeatSec();              //スイングの進捗ミリ秒
-                const swingProgressRate = (swingProgress/this.oneBeatSec())*100;    //スイングの進捗割合
+                const swingProgress = this.elapsedTime % this.oneBeatSec(); //スイングの進捗ミリ秒
+                const swingProgressRate = (swingProgress / this.oneBeatSec()) * 100; //スイングの進捗割合
                 let angle;
-            
-                if(swingProgressRate < 50) angle = (swingAngle/2)-((swingAngle/2)*((swingProgressRate*2)/100));
-                else if(swingProgressRate > 50) angle = -(swingAngle/2)*(((swingProgressRate-50)*2)/100);
-                else if (angle == 0) angle = (swingAngle/2);
+
+                if (swingProgressRate < 50) angle = (swingAngle / 2) - ((swingAngle / 2) * ((swingProgressRate * 2) / 100));
+                else if (swingProgressRate > 50) angle = -(swingAngle / 2) * (((swingProgressRate - 50) * 2) / 100);
+                else if (angle == 0) angle = (swingAngle / 2);
                 else angle = 0;
-                if(((Math.floor(this.elapsedTime/this.oneBeatSec()))%2)==1){
-                    angle = angle*-1;
+                if (((Math.floor(this.elapsedTime / this.oneBeatSec())) % 2) == 1) {
+                    angle = angle * -1;
                 }
                 return Number(angle);
             },
-            beatNow(){
-                return (this.beatTime+this.oneMusicalBar)%this.oneMusicalBar;
+            beatNow() {
+                return (this.beatTime + this.oneMusicalBar) % this.oneMusicalBar;
             },
-            beatAngle2(){
+            beatAngle2() {
                 return this.beatAngle;
             }
         },
         methods: {
             //一拍の秒数
             oneBeatSec() {
-                const sec = (60/this.bpm)*1000;//ミリ秒
+                const sec = (60 / this.bpm) * 1000; //ミリ秒
                 return sec;
             },
             //beatが鳴った回数
             //beatTime(){
             //    return Math.floor(this.elapsedTime/this.oneBeatSec());  
             //},
-            
-           // swing(){
-           //     const swingProgress = this.elapsedTime%this.oneBeatSec();              //スイングの進捗ミリ秒
-           //     const swingProgressRate = (swingProgress/this.oneBeatSec())*100;    //スイングの進捗割合
-           //     
-           //     
-           //     
-           // },
-            
-            
+
+            // swing(){
+            //     const swingProgress = this.elapsedTime%this.oneBeatSec();              //スイングの進捗ミリ秒
+            //     const swingProgressRate = (swingProgress/this.oneBeatSec())*100;    //スイングの進捗割合
+            //     
+            //     
+            //     
+            // },
+
+
             //再描画間隔
             rerenderInterval() {
-                const interval = 1000/this.fps;
+                const interval = 1000 / this.fps;
                 return interval;
             },
             //再帰的に使える用の関数
             elapsedTimeCountUp() {
-                if(this.isMetronomeRunning){
+                if (this.isMetronomeRunning) {
                     const interval = this.rerenderInterval();
-                    window.setTimeout(() =>{
-                        this.elapsedTime = this.elapsedTime+interval;
+                    window.setTimeout(() => {
+                        this.elapsedTime = this.elapsedTime + interval;
                         this.elapsedTimeCountUp();
-                    },interval);
+                    }, interval);
                 }
             },
             soundBeat() {
                 let beatVolume = 0.5;
-                console.log(this.oneMusicalBar);
-                if(this.oneMusicalBar!=0 && (this.beatTime%this.oneMusicalBar)==0 && this.soundMusicalBar){
+                if (this.oneMusicalBar != 0 && (this.beatTime % this.oneMusicalBar) == 0 && this.soundMusicalBar) {
                     beatVolume = 1;
                 }
                 source = audioContext.createBufferSource();
@@ -127,37 +126,36 @@ let gainNode;
                     source.start(0);
                 });
             },
-            beatAngleCount(){
-                this.beatAngle = this.beatAngle+(360/this.oneMusicalBar);
+            beatAngleCount() {
+                this.beatAngle = this.beatAngle + (360 / this.oneMusicalBar);
             },
             beatLoop() {
-                if(this.isMetronomeRunning){
-                   // this.beat.play();
+                if (this.isMetronomeRunning) {
+                    // this.beat.play();
                     this.beatTime++;
-                    this.elapsedTime = this.beatTime*this.oneBeatSec();
-                    console.log((this.beatTime*this.oneBeatSec())-this.elapsedTime);
-                   // this.elapsedTime = this.beatTime*this.oneBeatSec();
+                    this.elapsedTime = this.beatTime * this.oneBeatSec();
+                    // this.elapsedTime = this.beatTime*this.oneBeatSec();
                     this.beatAngleCount();
                     this.soundBeat();
-                    
+
                     const interval = this.oneBeatSec();
-                    window.setTimeout(() =>{
-                        
+                    window.setTimeout(() => {
+
                         this.beatLoop();
-                    },interval);
+                    }, interval);
                 }
             },
             setBeat() {
                 audioContext = new AudioContext;
                 textfile = fs.readFileSync('./public/beat.mp3', (err) => {
-                        if (err) throw err;
-                    });
+                    if (err) throw err;
+                });
                 toArrayBuffer = function(buf) {
                     return buf.buffer.slice(buf.byteOffset, buf.byteOffset + buf.byteLength);
                 }
             },
             runMetronome() {
-                if(!this.isMetronomeRunning){
+                if (!this.isMetronomeRunning) {
                     this.isMetronomeRunning = true;
                     this.setBeat();
                     this.elapsedTimeCountUp();
@@ -165,7 +163,7 @@ let gainNode;
                 }
             },
             stopMetronome() {
-                if(this.isMetronomeRunning){
+                if (this.isMetronomeRunning) {
                     this.isMetronomeRunning = false;
                     this.beatTime = 0;
                     this.beatAngle = 0;
@@ -174,79 +172,86 @@ let gainNode;
             },
 
         },
-        mounted(){
+        mounted() {
             //this.elapsedTimeCountUp();
             //this.setBeat();
             //this.beatLoop();
         },
-        watch:{
-            oneMusicalBar: function(){
+        watch: {
+            oneMusicalBar: function() {
 
-                this.beatAngle=((this.beatTime+this.oneMusicalBar)%this.oneMusicalBar)*(360/this.oneMusicalBar);
+                this.beatAngle = ((this.beatTime + this.oneMusicalBar) % this.oneMusicalBar) * (360 / this.oneMusicalBar);
             }
         }
     };
 </script>
 
 <style lang="scss" scoped>
-    .metronome-wrapper{
+    .metronome-wrapper {
         display: flex;
         flex-direction: column;
         align-items: center;
-        padding:20px;
-        .musicalbar-view{
+        padding: 20px;
+
+        .musicalbar-view {
             display: flex;
             justify-content: space-around;
             position: relative;
-            .one-beat{
+
+            .one-beat {
                 border-radius: 100%;
-                height:10px;
-                width:10px;
-                background:red;
+                height: 10px;
+                width: 10px;
+                background: red;
             }
-            .one-beat2{
+
+            .one-beat2 {
                 border-radius: 100%;
-                height:10px;
-                width:10px;
-                background:red;
+                height: 10px;
+                width: 10px;
+                background: red;
                 position: absolute;
-                
+
             }
-            .beat-now{
-                background:black;
+
+            .beat-now {
+                background: black;
             }
         }
-        .hand-wrapper{
-            height:200px;
+
+        .hand-wrapper {
+            height: 200px;
             position: relative;
+
             .hand {
                 position: absolute;
                 left: 150px;
                 top: 20px;
-                background:red;
+                background: red;
             }
         }
-        .handball-wrapper{
-            display:flex;
+
+        .handball-wrapper {
+            display: flex;
             flex-direction: column;
             align-items: center;
-            position:relative;
+            position: relative;
+
             .hand-ball {
                 position: absolute;
-                border-radius:100%;
-                background:red;
+                border-radius: 100%;
+                background: red;
                 width: 20px;
                 height: 20px;
                 transform-origin: 10px 210px;
-                top:90px;
-                background: rgba( 62, 62, 62, 0.50 );
-                box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
-                backdrop-filter: blur( 5.0px );
-                -webkit-backdrop-filter: blur( 5.0px );
+                top: 90px;
+                background: rgba(62, 62, 62, 0.50);
+                box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+                backdrop-filter: blur(5.0px);
+                -webkit-backdrop-filter: blur(5.0px);
                 border-radius: 10px;
-                border: 1px solid rgba( 255, 255, 255, 0.18 );
+                border: 1px solid rgba(255, 255, 255, 0.18);
             }
         }
     }
-
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,149 +1,188 @@
 <template>
-  <div class="home">
-    <div class="metronome">
-        <Metronome ref="metronome" :bpm="bpm" :oneMusicalBar="oneMusical.bar" :soundMusicalBar = "soundMusicalBar"/>
+    <div class="home">
+        <div class="metronome">
+            <Metronome ref="metronome" :bpm="bpm.bpm" :oneMusicalBar="oneMusical.bar" :soundMusicalBar="soundMusicalBar" />
+        </div>
+        <div class="metronome-controller">
+            <div :class="['playback' ,{'pause': playbackNow}]" @click="suspend()"></div>
+            <label>
+                <span>bpm</span>
+                <input type="number" min="1" class="textbox" v-model="bpm.bpm">
+                <input type="range" name="bpm" class="slider" :class="{'danger': bpm.isDanger}" v-model="bpm.bpm" :min="bpm.min" :max="bpm.max">
+            </label>
+            <label>
+                <input type="checkbox" v-model="soundMusicalBar" />
+                <input type="number" min="1" class="textbox" v-model="oneMusical.bar">拍子毎
+                <input type="range" name="beat" class="slider" :class="{'danger': oneMusical.isDanger}" v-model="oneMusical.bar" :min="oneMusical.min" :max="oneMusical.max">
+            </label>
+        </div>
     </div>
-    <div class="metronome-controller">
-        <div :class="['playback' ,{'pause': playbackNow}]" @click="suspend()"></div>
-        <label>
-            <span>bpm</span>
-            <input type="text" class="textbox" v-model="bpm.bpm">
-            🐢 
-            <input type="range" name="bpm" class="slider" :class="{'danger': bpm.isDanger}" v-model="bpm.bpm" :min="bpm.min" :max="bpm.max">
-            🐰
-        </label>
-        <label>
-            <input type="checkbox" v-model="soundMusicalBar"/>
-            <input type="text" class="textbox" v-model="oneMusical.bar">拍子毎
-            <input type="range" name="beat" class="slider" :class="{'danger': oneMusical.isDanger}" v-model="oneMusical.bar" :min="oneMusical.min" :max="oneMusical.max">
-        </label>
-    </div>
-    {{oneMusical.isDanger}}
-  </div>
 </template>
 
 <script>
-import Metronome from '../components/Metronome.vue'; 
-//import flexibleTextbox from '../components/flexibleTextbox.vue'; 
-//import Polygon from '../components/polygon.vue'; 
-    
-    export default{
+    import Metronome from '../components/Metronome.vue';
+    //import flexibleTextbox from '../components/flexibleTextbox.vue'; 
+    //import Polygon from '../components/polygon.vue'; 
+
+    export default {
         name: 'app',
         components: {
             Metronome,
-          //  flexibleTextbox
-         //   Polygon
+            //  flexibleTextbox
+            //   Polygon
         },
         data() {
             return {
-                bpm:{
-                    bpm:60,
-                    min:1,
-                    max:300,
-                    isDanger:false
+                bpm: {
+                    bpm: 60,
+                    min: 1,
+                    max: 300,
+                    isDanger: false
                 },
-                oneMusical:{
-                    bar:4,
-                    min:2,
-                    max:14,
-                    isDanger:false
+                oneMusical: {
+                    bar: 4,
+                    min: 2,
+                    max: 14,
+                    isDanger: false
                 },
-                musicalBarVals:[2,3,4,5,6,7,8,9,10,11,12,13,14],
-                vertex:5,
-                soundMusicalBar:true,
-                playbackNow:false,
+                musicalBarVals: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+                vertex: 5,
+                soundMusicalBar: true,
+                playbackNow: false,
             };
         },
-        methods:{
+        methods: {
             suspend() {
-                if(this.playbackNow){
+                if (this.playbackNow) {
                     this.playbackNow = false;
                     this.$refs.metronome.stopMetronome();
-                }
-                else{
+                } else {
                     this.playbackNow = true;
                     this.$refs.metronome.runMetronome();
-                } 
+                }
             },
 
         },
-        watch:{
-            'oneMusical.bar'(){
-                if(this.oneMusical.bar>this.oneMusical.max)this.oneMusical.isDanger = true;
+        watch: {
+            'oneMusical.bar'() {
+                if (this.oneMusical.bar > this.oneMusical.max) this.oneMusical.isDanger = true;
                 else this.oneMusical.isDanger = false;
             },
-            'bpm.bpm'(){
-                if(this.bpm.bpm>this.bpm.max)this.bpm.isDanger = true;
+            'bpm.bpm'() {
+                if (this.bpm.bpm > this.bpm.max) this.bpm.isDanger = true;
                 else this.bpm.isDanger = false;
             }
         }
     };
 </script>
 <style lang="scss">
-    .home{
+    .home {
         display: flex;
         flex-direction: column;
         justify-content: space-between;
         font-weight: bold;
-        color:white;
+        color: white;
         background: linear-gradient(-135deg, #E4A972, #39ACAC),
-        linear-gradient(75deg, #E4A972, #9941D8 ,#79ffff)fixed;
+            linear-gradient(75deg, #E4A972, #9941D8, #79ffff)fixed;
         border-radius: 15px;
-        padding:10px;
-        .metronome-controller{
+        padding: 10px;
+
+        .metronome-controller {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
             display: flex;
             flex-direction: column;
-            margin-top:10px;
-            padding:10px 30px;
-            background: rgba( 62, 62, 62, 0.50 );
-            box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
-            backdrop-filter: blur( 5.0px );
-            -webkit-backdrop-filter: blur( 5.0px );
+            align-items: center;
+            justify-content: space-around;
+            margin-top: 10px;
+            padding: 20px 30px;
+            height:150px;
+            background: rgba(62, 62, 62, 0.50);
+            box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+            backdrop-filter: blur(5.0px);
+            -webkit-backdrop-filter: blur(5.0px);
             border-radius: 10px;
-            border: 1px solid rgba( 255, 255, 255, 0.18 );
-            .textbox{
-                border:none;
-                background:none;
+            border: 1px solid rgba(255, 255, 255, 0.18);
+
+            .textbox {
+                border: none;
+                background: none;
                 font-weight: bold;
-                color:white;
-                width:40px;
+                color: white;
+                width: 40px;
             }
         }
     }
-.playback{
-    border-top: 15px solid transparent;
-    border-bottom: 15px solid transparent;
-    border-left: 30px solid #6aa7cd;
-    border-radius: 5px;
-    transition:  border-left 0.1s,  border-right 0.1s;
-}   
-.pause{
-    border-left: 10px solid #6aa7cd;
-    border-right: 10px solid #6aa7cd;
-    border-top: 0px solid transparent;
-    border-bottom: 0px solid transparent;
-    border-radius: 0px;
-    height:30px;
-    width:15px;
-    transition: border-left 0.1s, border-right 0.1s ;
-}
-    .slider{
+
+    .playback {
+        border-top: 15px solid transparent;
+        border-bottom: 15px solid transparent;
+        border-left: 30px solid #ddd;
+        border-radius: 5px;
+        transition: border-left 0.1s, border-right 0.1s;
+    }
+
+    .pause {
+        border-left: 10px solid #ddd;
+        border-right: 10px solid #ddd;
+        border-top: 0px solid transparent;
+        border-bottom: 0px solid transparent;
+        border-radius: 0px;
+        height: 30px;
+        width: 15px;
+        transition: border-left 0.1s, border-right 0.1s;
+    }
+
+    .slider {
         -webkit-appearance: none; // これ無しだとスタイルがほぼ全く反映されないので注意
         appearance: none;
         cursor: pointer; // カーソルを分かりやすく
-        background: #8acdff; // バーの背景色
-        height: 14px; // バーの高さ
+        background: #ddd; // バーの背景色
+        height: 3px; // バーの高さ
         width: 100%; // スライダーの幅
         border-radius: 10px; // バーの端の丸み
-        border: solid 3px #dff1ff; // バーまわりの線
-        outline: 0; /* アウトラインを消して代わりにfocusのスタイルをあてる */
+        border: none; // バーまわりの線
+        outline: 0;
+
+        /* アウトラインを消して代わりにfocusのスタイルをあてる */
         &:focus {
-            box-shadow: 0 0 3px rgb(0, 161, 255);
+            box-shadow: 0 0 3px rgb(200, 200, 200);
+        }
+
+        // -webkit-向けのつまみ
+        &::-webkit-slider-thumb {
+            -webkit-appearance: none; // デフォルトのつまみのスタイルを解除
+            background: #ddd; // 背景色
+            width: 24px; // 幅
+            height: 24px; // 高さ
+            border-radius: 50%; // 円形に
+            box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.15); // 影
+        }
+
+        // -moz-向けのつまみ
+        &::-moz-range-thumb {
+            background: #ddd; // 背景色
+            width: 24px; // 幅
+            height: 24px; // 高さ
+            border-radius: 50%; // 円形に
+            box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.15); // 影
+            border: none; // デフォルトの線を消す
+        }
+
+        // Firefoxで点線が周りに表示されてしまう問題の解消
+        &::-moz-focus-outer {
+            border: 0;
+        }
+
+        // つまみをドラッグしているときのスタイル
+        &:active::-webkit-slider-thumb {
+            box-shadow: 0px 5px 10px -2px rgba(0, 0, 0, 0.3);
         }
     }
-    .danger{
-        background:red;
+
+    .danger {
+        background: red;
     }
-
-
 </style>
